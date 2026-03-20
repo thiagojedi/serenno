e489f906ed9909d0e609055dc0de0faf66fc4f52
Thiago 'Jedi' Cerqueira <thiagoa7@gmail.com>
2026-03-19 23:28:13 -0300

    Fix build

--- build/10-build.sh
+++ build/10-build.sh
@@ -14,26 +14,30 @@ echo "::group:: Install Sway Desktop"
 mkdir -p /var/lib/apt/lists/partial
 mkdir -p /var/lib/dpkg/
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Update package lists
 apt-get update
-apt-get install -y debconf
-
-export DEBIAN_FRONTEND=noninteractive
+apt-get install -y \
+	debconf \
+	libgio-2.0-dev \
+	python3
 
 # Install sway
 apt-get install -y \
-    foot-themes \
-    sway \
-    sway-backgrounds \
-    swayidle \
-    swaylock \
-    xdg-desktop-portal-gtk \
-    xdg-desktop-portal-wlr
-
+	foot-themes \
+	sway \
+	sway-backgrounds \
+	swayidle \
+	swaylock \
+	xdg-desktop-portal-gtk \
+	xdg-desktop-portal-wlr \
+	lightdm
 
 apt-get install -y \
-    podman \
-    xwayland
+	podman \
+	thunar \
+	network-manager-applet
 
 apt-get install -y whois
 usermod -p "$(echo "changeme" | mkpasswd -s)" root
@@ -47,26 +51,26 @@ echo "::group:: Copy Custom Files"
 # Copy Brewfiles to standard location (if using homebrew)
 mkdir -p /usr/share/ublue-os/homebrew/
 if [ -d /ctx/custom/brew ]; then
-    cp /ctx/custom/brew/*.Brewfile /usr/share/ublue-os/homebrew/ 2>/dev/null || true
+	cp /ctx/custom/brew/*.Brewfile /usr/share/ublue-os/homebrew/ 2>/dev/null || true
 fi
 
 # Consolidate Just Files (if using ujust)
 mkdir -p /usr/share/ublue-os/just/
 if [ -d /ctx/custom/ujust ]; then
-    find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just 2>/dev/null || true
+	find /ctx/custom/ujust -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >>/usr/share/ublue-os/just/60-custom.just 2>/dev/null || true
 fi
 
 # Copy Flatpak preinstall files (if using flatpak)
 mkdir -p /etc/flatpak/preinstall.d/
 if [ -d /ctx/custom/flatpaks ]; then
-    cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/ 2>/dev/null || true
+	cp /ctx/custom/flatpaks/*.preinstall /etc/flatpak/preinstall.d/ 2>/dev/null || true
 fi
 
 echo "::endgroup::"
 
 echo "::group:: System Configuration"
 
-cp /ctx/custom/os-release /usr/lib/os-release 
+cp /ctx/custom/os-release /usr/lib/os-release
 
 cat /usr/lib/os-release
 
